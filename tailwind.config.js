791aa7591f007b82c35b21b46ef73660c4f2202a
rezvan hassan zade <rezvan.1886.hzz@gmail.com>
2024-07-17 21:30:04 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
+module.exports = {
+  content: ["./puplic/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Vazirmatn"]
+      }
+    },
+  },
+  plugins: [],
+};
